40c252e6ee82d4952fb8128015384901487bc097
wikky2012 <wicliferono@gmail.com>
2020-07-18 21:27:03 +0300

    add event.preventDefault() function that keeps the text printed out uncleared

--- js/register.js
+++ js/register.js
@@ -2,23 +2,24 @@ var maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
 var femaleNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
 
 function calculate(){
+    event.preventDefault();
     var date = parseInt(document.getElementById('date').value);
     var month = parseInt(document.getElementById('month').value);
     var year = document.getElementById('year').value;
     var gender = document.getElementById('gender').value;
     if(date<01 || date>31){
-       alert('invalid date');
+      document.getElementById('output').innerHTML=('Invalid date');
        return;
     }
     
     if(month<01 || month>12){
-        alert('nvalid Month');
+      document.getElementById('output').innerHTML=('Invalid Month');
         return;
      }         
     
-     if(year<0001 || year>2020){
-        alert('Invalid year');
-        return false;
+     if(year<0001 || year>2020 ){
+      document.getElementById('output').innerHTML=('Invalid year');
+        return;
      }
 
      var cc = parseInt(year.slice(0,2));
@@ -27,47 +28,48 @@ function calculate(){
      d = (( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(month+1)/10)) + date ) % 7).toFixed(); 
 
      if(parseInt(d) === parseInt(0) && (gender==="male")){
-      alert('Your Akan name is: '+maleNames[0]);
+      document.getElementById('output').innerHTML=('You were born on Sunday and your Akan name is: '+maleNames[0]);
   }
   else if(parseInt(d) === parseInt(1) && (gender==="male")){
-     alert('Your Akan name is: '+maleNames[1]);
+   document.getElementById('output').innerHTML=('You were born on Monday and your Akan name is: '+maleNames[1]);
   }
   else if(parseInt(d) === parseInt(2) && (gender==="male")){
-     alert('Your Akan name is: '+maleNames[2]);
+   document.getElementById('output').innerHTML=('You were  born on Tuesday and your  Akan name is: '+maleNames[2]);
   }
   else if(parseInt(d) === parseInt(3) && (gender==="male")){
-     alert('Your Akan name is: '+maleNames[3]);
+   document.getElementById('output').innerHTML=('You were  born on Wednesday and your  Akan name is: '+maleNames[3]);
   }
   else if(parseInt(d) === parseInt(4) && (gender==="male")){
-     alert('Your Akan name is: '+maleNames[4]);
+   
+     document.getElementById('output').innerHTML='You were  born on Thursday and your Akan name is: '+maleNames[4];
   }
   else if(parseInt(d) === parseInt(5) && (gender==="male")){
-     alert('Your Akan name is: '+maleNames[5]);
+   document.getElementById('output').innerHTML=('You were  born on Friday and your  Akan name is: '+maleNames[5]);
   }
   else if(parseInt(d) === parseInt(6) && (gender==="male")){
-     alert('Your Akan name is: '+maleNames[6]);
+   document.getElementById('output').innerHTML=('You were  born on Saturday and your  Akan name is: '+maleNames[6]);
   }
 
       if(parseInt(d) === parseInt(0) && (gender==="female")){
-         alert('Your Akan name is: '+femaleNames[0]);
+         document.getElementById('output').innerHTML=('You were born on Sunday and your Akan name is: '+femaleNames[0]);
      }
      else if(parseInt(d) === parseInt(1) && (gender==="female")){
-        alert('Your Akan name is: '+femaleNames[1]);
+      document.getElementById('output').innerHTML=('You were born on Monday and your Akan name is: '+femaleNames[1]);
      }
      else if(parseInt(d) === parseInt(2) && (gender==="female")){
-        alert('Your Akan name is: '+femaleNames[2]);
+      document.getElementById('output').innerHTML=('You were  born on Tuesday and your  Akan name is: '+femaleNames[2]);
      }
      else if(parseInt(d) === parseInt(3) && (gender==="female")){
-        alert('Your Akan name is: '+femaleNames[3]);
+      document.getElementById('output').innerHTML=('You were  born on Wednesday and your  Akan name is: '+femaleNames[3]);
      }
      else if(parseInt(d) === parseInt(4) && (gender==="female")){
-        alert('Your Akan name is: '+femaleNames[4]);
+      document.getElementById('output').innerHTML=('You were  born on Thursday and your Akan name is: '+femaleNames[4]);
      }
      else if(parseInt(d) === parseInt(5) && (gender==="female")){
-        alert('Your Akan name is: '+femaleNames[5]);
+      document.getElementById('output').innerHTML=('You were  born on Friday and your  Akan name is: '+femaleNames[5]);
      }
      else if(parseInt(d) === parseInt(6) && (gender==="female")){
-        alert('Your Akan name is: '+femaleNames[6]);
+      document.getElementById('output').innerHTML=('You were  born on Saturday and your  Akan name is: '+femaleNames[6]);
      }
      else{
          return false;
