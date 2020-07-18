af9311da22c5f5dd5eb6297359e1df0b67ae53fa
wikky2012 <wicliferono@gmail.com>
2020-07-18 18:44:48 +0300

    add functionality to choose gender and day one was born simultaneously

--- js/register.js
+++ js/register.js
@@ -1,69 +1,93 @@
 var maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
 var femaleNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
-    
 
 function calculate(){
-    var date=parseInt(document.getElementById('date').value);
-    var month=parseInt(document.getElementById('month').value);
-    var year=document.getElementById('year').value;
-    var gender=document.getElementsByName('gender').value;
-    if(date<1 || date>31){
+    var date = parseInt(document.getElementById('date').value);
+    var month = parseInt(document.getElementById('month').value);
+    var year = document.getElementById('year').value;
+    var gender = document.getElementById('gender').value;
+    if(date<01 || date>31){
        alert('invalid date');
        return;
     }
     
-    if(month<1 || month>12){
-        alert('invalid Month');
+    if(month<01 || month>12){
+        alert('nvalid Month');
         return;
-     }
+     }         
     
-     if(year<1 || year>2020){
-        alert('invalid year');
-        return;
+     if(year<0001 || year>2020){
+        alert('Invalid year');
+        return false;
      }
-    
+
      var cc = parseInt(year.slice(0,2));
-     var yy =parseInt(year.slice(2,4));
+     var yy = parseInt(year.slice(2,4));
      var d;
      d = (( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(month+1)/10)) + date ) % 7).toFixed(); 
-    
+      
      
-     if(parseInt(d)===parseInt(0)){
-         alert('You were born in Sunday');
+   //  if(gender === 'male'){
+   //     return;
+   //  }
+   //  else if(gender === 'female'){
+   //      return;
+   //  }
+   //  else if(gender === 'other'){
+   //      alert('Double Gender');
+   //  }
+   //  else{
+   //      alert('Please provide the gender');
+   //   }
+
+     if(parseInt(d) === parseInt(0) && (gender==="male")){
+      alert('Your Akan name is: '+maleNames[0]);
+  }
+  else if(parseInt(d) === parseInt(1) && (gender==="male")){
+     alert('Your Akan name is: '+maleNames[1]);
+  }
+  else if(parseInt(d) === parseInt(2) && (gender==="male")){
+     alert('Your Akan name is: '+maleNames[2]);
+  }
+  else if(parseInt(d) === parseInt(3) && (gender==="male")){
+     alert('Your Akan name is: '+maleNames[3]);
+  }
+  else if(parseInt(d) === parseInt(4) && (gender==="male")){
+     alert('Your Akan name is: '+maleNames[4]);
+  }
+  else if(parseInt(d) === parseInt(5) && (gender==="male")){
+     alert('Your Akan name is: '+maleNames[5]);
+  }
+  else if(parseInt(d) === parseInt(6) && (gender==="male")){
+     alert('Your Akan name is: '+maleNames[6]);
+  }
+  else{
+      alert('Invalid');
+
+      if(parseInt(d) === parseInt(0) && (gender==="female")){
+         alert('Your Akan name is: '+femaleNames[0]);
      }
-     else if(parseInt(d)===parseInt(1)){
-        alert('You were born in Monday');
+     else if(parseInt(d) === parseInt(1) && (gender==="female")){
+        alert('Your Akan name is: '+femaleNames[1]);
      }
-     else if(parseInt(d)===parseInt(2)){
-        alert('You were born in Tuesday');
+     else if(parseInt(d) === parseInt(2) && (gender==="female")){
+        alert('Your Akan name is: '+femaleNames[2]);
      }
-     else if(parseInt(d)===parseInt(3)){
-        alert('You were born in Wednesday');
+     else if(parseInt(d) === parseInt(3) && (gender==="female")){
+        alert('Your Akan name is: '+femaleNames[3]);
      }
-     else if(parseInt(d)===parseInt(4)){
-        alert(maleNames[0]);
+     else if(parseInt(d) === parseInt(4) && (gender==="female")){
+        alert('Your Akan name is: '+femaleNames[4]);
      }
-     else if(parseInt(d)===parseInt(5)){
-        alert('You were born in Friday');
+     else if(parseInt(d) === parseInt(5) && (gender==="female")){
+        alert('Your Akan name is: '+femaleNames[5]);
      }
-     else if(parseInt(d)===parseInt(6)){
-        alert('You were born in Saturday');
+     else if(parseInt(d) === parseInt(6) && (gender==="female")){
+        alert('Your Akan name is: '+femaleNames[6]);
      }
      else{
          alert('Invalid');
-     }
-    
-   
-    if(gender==='male'){
-        alert('male');
-    }
-    else if(gender==='female'){
-        alert('female');
-    }
-    else if(gender=='other'){
-        alert('Double Gender');
-    }
-    else{
-        alert("Please provide the gender");
-    }
+  }
+
+  }
 }
