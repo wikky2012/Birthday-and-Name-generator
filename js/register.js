f39d0aece7cabfa8d9a778ebb7d3b9d5224bf672
wikky2012 <wicliferono@gmail.com>
2020-07-18 11:25:20 +0300

    add a function to calculate day of the week

--- js/register.js
+++ js/register.js
@@ -1,10 +1,12 @@
 var maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
 var femaleNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+    
 
 function calculate(){
     var date=parseInt(document.getElementById('date').value);
     var month=parseInt(document.getElementById('month').value);
-    var year=parseInt(document.getElementById('year').value);
+    var year=document.getElementById('year').value;
+    
     if(date<1 || date>31){
        alert('invalid date');
     }
@@ -14,7 +16,11 @@ function calculate(){
      if(year<1 || year>2020){
         alert('invalid year');
      }
-    
-    
+     var cc = parseInt(year.slice(0,2));
+     var yy =parseInt(year.slice(2,4));
+     var d;
+     d = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(month+1)/10)) + date ) % 7;
+     alert(d.toFixed());
 
+     
 }
